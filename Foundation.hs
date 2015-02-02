0ca7b28c485782f7034f835bb3ffd121f62df9fe
chansuke <moonset20@gmail.com>
2015-02-03 00:02:33 +0900

    add files and templates

--- Foundation.hs
+++ Foundation.hs
@@ -1,18 +1,23 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
-
 module Foundation where
 
 import Control.Concurrent.STM
 import Data.ByteString.Lazy(ByteString)
+import Data.Default
 import Data.Text (Text)
+import qualified Data.Text as Text
+import Text.Hamlet
 
 import Yesod
+import Yesod.Default.Util
 
 data App = App(TVar[(Text, ByteString)])
-instance Yesod App
+
+--instance Yesod App
+
+instance Yesod App where
+  defaultLayout widget = do
+    pc <- widgetToPageContent $ $(widgetFileNoReload def "default-layout")
+    withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
 instance RenderMessage App FormMessage where
   renderMessage _ _ = defaultFormMessage
@@ -29,3 +34,11 @@ addFile :: App -> (Text, ByteString) -> Handler ()
 addFile (App tstore) op =
   liftIO . atomically $ do
       modifyTVar tstore $ \ ops -> op : ops
+
+getById :: Text -> Handler ByteString
+getById ident = do
+    App tstore <- getYesod
+    operations <- liftIO $ readTVarIO tstore
+    case lookup ident operations of
+      Nothing -> notFound
+      Just bytes -> return bytes
