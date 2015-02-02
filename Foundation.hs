bf98329baddeafcb0b17a2fce020102ea380525a
chansuke <moonset20@gmail.com>
2015-02-02 22:30:04 +0900

    update

--- Foundation.hs
+++ Foundation.hs
@@ -1,8 +1,30 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Foundation where
+
+import Control.Concurrent.STM
+import Data.Text (Text)
 
 import Yesod
 
-data App = App
+-- data App = App
+data App = App(TVar[Text]
 instance Yesod App
 
+instance RenderMessage App FormMessage where
+  renderMessage _ _ = defaultFormMessage
+
 mkYesodData "App" $(parseRoutesFile "config/routes")
+
+getList :: Handler [Text]
+getList = do
+    App tstate <- getYesod
+    liftIO $ readTVarIO tstate
+
+addFile :: App -> Text -> Handler ()
+addFile (App tstore) op =
+    liftIO . atomically $ do
+        modifyTVar tstore $ \ ops -> op : ops
--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,40 +1,28 @@
 module Handler.Home where
 
-import Import
-import Yesod.Form.Bootstrap3
-    ( BootstrapFormLayout (..), renderBootstrap3, withSmallInput )
+import Data.Default
+import Yesod
+import Yesod.Default.Util
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
+import Foundation
+
 getHomeR :: Handler Html
 getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe (FileInfo, Text)
-        handlerName = "getHomeR" :: Text
+    (formWidget, formEncType) <- generateFromPost uploadForm
+    filenames <- getList
     defaultLayout $ do
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
+        setTitle "File Processor"
+        $(widgetFileNoReload def "home")
 
 postHomeR :: Handler Html
 postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
+  ((result, _),_) <- runFormPost uploadForm
+  case result of
+  FormSuccess fi -> do
+    app <- getYesod
+    addFile app $ fileName fi
+  _ -> return ()
+  redirect HomeR
 
-    defaultLayout $ do
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
+uploadForm = renderDivs $ fileAFormReq "file"
 
-sampleForm :: Form (FileInfo, Text)
-sampleForm = renderBootstrap3 BootstrapBasicForm $ (,)
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField (withSmallInput "What's on the file?") Nothing
